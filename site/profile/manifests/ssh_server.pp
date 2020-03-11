a455f636db9e0cedf65f5758da09411e561bdd61
alswi <alswi@users.noreply.github.com>
2020-03-11 22:26:06 +0800

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDi8JapegLeGX6yklhU1FLElGIYPqfi7VX+Qbd/lU96eLGC6kp6Dl5uhRXq0w1UNW1f88dgDjW3/E+nR8NphKyo9pYSK4B9AEsT/beT6UdL6yVIXbs7oLv89JI81ltKfDZIEa9W1tLcqJMSk6D2/CqxcLat0BOUdrjgnU4eMk4G1K6xynHdZvAjC0JT68Cp0TlM/Wb0r/4dDruX8EsM+lPYfPi2Y6UJS5L96xmEZQMvrCjZUbYiWufqTsg1HO+6exj2QOLdJT+9XIa1cH8BtSY41lCK/O8+tD0ZLqFmpCPRjFtbQpoEk92tjD30+Bulsa+6lw6ABQs1g4YA9IPn+m1p',
 	}  
 }
